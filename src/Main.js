4928a7abb383e4f395ad9cc364923bc12db98b4b
torontopubliclibra <dana.r.teagle@gmail.com>
2022-10-26 21:48:31 -0400

    fixed deck shuffle function and altered styles

--- src/Main.js
+++ src/Main.js
@@ -9,16 +9,38 @@ const Main = () => {
     const [ gameStart, setGameStart ] = useState(false);
     const [ playerName, setPlayerName ] = useState("");
 
+    const shuffle = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    };
+
     useEffect(() => {
         const database = getDatabase(firebase);
         const dbRef = ref(database, `/characters`);
 
         onValue(dbRef, (response) => {
+            const allCards = [];
             const newDeck = [];
             const deck = response.val();
 
             for (let character in deck){
-                newDeck.push({key: character, data: deck[character]});
+                allCards.push({key: character, data: deck[character]});
+            }
+
+            shuffle(allCards);
+
+            let i = 1;
+            for (let character in allCards){
+                newDeck.push(allCards[character]);
+                i++
+                if (i === 21){
+                    break;
+                }
             }
 
             setCharacterDeck(newDeck);
