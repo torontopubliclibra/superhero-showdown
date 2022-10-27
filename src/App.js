fc70fe4e6db304909a8a0c4519e053348d73713f
torontopubliclibra <dana.r.teagle@gmail.com>
2022-10-27 10:08:20 -0400

    started mobile styling

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Main from './Main'
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Superhero Showdown</h1>
-        <h2>Who will win the civil war between heroes?</h2>
-      </header>
-      <Main />
-      <footer>
-        <p>All characters and data pulled from the <a href="https://marvel.fandom.com/wiki/Marvel_Database">Marvel Database</a> and stored using <a href="https://firebase.google.com/">Firebase</a>.</p>
-        <p>Built by <a href="https://danateagle.com" target="_blank">Dana Teagle</a> at <a href="https://junocollege.com" target="_blank">Juno College</a> in 2022.</p>
-      </footer>
+      <div className="wrapper">
+        <header>
+          <h1>Superhero Showdown</h1>
+          <h2>Who will win the civil war between heroes?</h2>
+        </header>
+        <Main />
+        <footer>
+          <p>All characters and data pulled from the <a href="https://marvel.fandom.com/wiki/Marvel_Database">Marvel Database</a> and stored using <a href="https://firebase.google.com/">Firebase</a>.</p>
+          <p>Built by <a href="https://danateagle.com" target="_blank">Dana Teagle</a> at <a href="https://junocollege.com" target="_blank">Juno College</a> in 2022.</p>
+        </footer>
+      </div>
     </div>
   );
 }
